bdc68d6c92ef3b1243892839be30d790e629da31
Uncle Sam <djiasara@gmail.com>
2019-04-18 19:58:20 -0400

    implement the basic logic of the game Plus styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,30 @@
 
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     const ListGame = [
-        {img:"mango.jpg", desc:"i am a fruit", wordToGuess:"mango"}, 
-        {img:"apple.jpg", desc:"i am a fruit", wordToGuess:"apple"},
-        {img:"pineapple.jpg", desc:"i am a fruit", wordToGuess:"pineapple"},
-        {img:"house.jpg", desc:"i am a house", wordToGuess:"house"}
+        { img: "mango.jpg", desc: "i am a fruit", wordToGuess: "mango" },
+        { img: "apple.jpg", desc: "i am a fruit", wordToGuess: "apple" },
+        { img: "pineapple.jpg", desc: "i am a fruit", wordToGuess: "pineapple" },
+        { img: "house.jpg", desc: "i am a house", wordToGuess: "house" }
     ];
 
-    document.onkeyup(function(event){
-        let currentObject = ListGame[Math.floor(Math.random()* ListGame.length)];
-        $(".image").attr("src", "assets/images/"+ currentObject.img);
-        $(".desc").html(currentObject.desc);
-        $(".currentWord").html(currentObject.wordToGuess);
-    })
-})
+    let currentObject = {};
+    document.onkeyup = (function (event) {
+        if(event.key === "Enter"){
+            $('.startGame').text("Game on!");
+            $('.startGame').after(`<p>could you guessed the word below?</p>`);
+            let currentObject = ListGame[Math.floor(Math.random() * ListGame.length)];
+            $(".card-img-top").attr("src", "assets/images/" + currentObject.img);
+            $(".desc").html(currentObject.desc);
+            $(".currentWord").html(currentObject.wordToGuess);
+        }
+        
+    });
+
+    
+
+
+});
 
 
 
