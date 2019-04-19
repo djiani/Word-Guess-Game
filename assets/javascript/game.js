23bf167e5eb306a626fd0c9a4f9a28c04e36a2de
Uncle Sam <djiasara@gmail.com>
2019-04-18 20:43:37 -0400

    implement getWordToGuess function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,15 @@
 
 $(document).ready(function () {
 
+    //replace each letter of the word by -
+    function getWordToGuess(str){
+        let strOut = "";
+        for(i=0; i<str.length; i++){
+            strOut +="- "
+        }
+        return strOut;
+    };
+
     const ListGame = [
         { img: "mango.jpg", desc: "i am a fruit", wordToGuess: "mango" },
         { img: "apple.jpg", desc: "i am a fruit", wordToGuess: "apple" },
@@ -9,14 +18,25 @@ $(document).ready(function () {
     ];
 
     let currentObject = {};
+    let isGameStarted = false;
+    let maxNumGuess = 0;
     document.onkeyup = (function (event) {
-        if(event.key === "Enter"){
+        if(event.key === "Enter" && !isGameStarted){
+            //Game initialization
+            isGameStarted = true; 
             $('.startGame').text("Game on!");
-            $('.startGameDesc').startGameDesc(`could you guessed the word below?`);
+            $('.startGameDesc').text(`could you guessed the word below?`);
             let currentObject = ListGame[Math.floor(Math.random() * ListGame.length)];
             $(".card-img-top").attr("src", "assets/images/" + currentObject.img);
             $(".desc").html(currentObject.desc);
-            $(".currentWord").html(currentObject.wordToGuess);
+            $(".currentWord").html(getWordToGuess(currentObject.wordToGuess));
+            maxNumGuess = currentObject.wordToGuess.length + 5; 
+            $(".TotalGuessRemaining").text(maxNumGuess);
+        }
+        else if(isGameStarted){
+            //collected user input and implement the core logic of the game here!
+
+            
         }
         
     });
