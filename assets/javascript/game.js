eb71ccc2737c912e36abbd17aadbd2f2f71b2992
Uncle Sam <djiasara@gmail.com>
2019-04-19 10:32:27 -0400

    implementing core logic of the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,13 +2,38 @@
 $(document).ready(function () {
 
     //replace each letter of the word by -
-    function getWordToGuess(str){
-        let strOut = "";
-        for(i=0; i<str.length; i++){
-            strOut +="- "
+    function printWordToGuess(displayWordToGuess, wordToGuess, userGuess) {
+        if (displayWordToGuess == "") {
+            for (let i = 0; i < wordToGuess.length; i++) {
+                displayWordToGuess += "-";
+            }
+        } else {
+            console.log(wordToGuess)
+            let temp = "";
+            for (let i = 0; i < wordToGuess.length; i++) {
+
+                console.log(wordToGuess[i] + " compare to " + userGuess);
+                if (wordToGuess[i] == userGuess) {
+                    temp += userGuess;
+                    //displayWordToGuess[i]= userGuess;
+                    //console.log(displayWordToGuess[i]);
+
+                } else {
+                    temp += displayWordToGuess[i];
+                }
+            }
+            console.log('temp: ' + temp);
+            displayWordToGuess = temp;
+            console.log('displayWordToGuess: ' + displayWordToGuess);
         }
-        return strOut;
+        return displayWordToGuess;
     };
+
+    function isUserGuest(userGuess, wordToGuess) {
+        return (wordToGuess.includes(userGuess))
+    }
+
+
 
     const ListGame = [
         { img: "mango.jpg", desc: "i am a fruit", wordToGuess: "mango" },
@@ -19,29 +44,75 @@ $(document).ready(function () {
 
     let currentObject = {};
     let isGameStarted = false;
-    let maxNumGuess = 0;
+    let numGuessRem = 0;
+    let displayWordToGuess = "";
+    let wordToGuess = "";
+    let userGuess = '';
+    let totalWins = 0;
+    let letterAlreadyGuess = [];
+
+    function playGame(str) {
+        let dashCount = 0;
+        for (let i = 0; i < str.length; i++) {
+            if (str[i] == '-') {
+                dashCount++;
+            }
+        }
+        return dashCount;
+    }
+
+
     document.onkeyup = (function (event) {
-        if(event.key === "Enter" && !isGameStarted){
+        if (event.key === "Enter" && !isGameStarted) {
             //Game initialization
-            isGameStarted = true; 
+            isGameStarted = true;
+            numGuessRem = 0;
+            displayWordToGuess = "";
+            wordToGuess = "";
+            letterAlreadyGuess = [];
             $('.startGame').text("Game on!");
             $('.startGameDesc').text(`could you guessed the word below?`);
             let currentObject = ListGame[Math.floor(Math.random() * ListGame.length)];
             $(".card-img-top").attr("src", "assets/images/" + currentObject.img);
             $(".desc").html(currentObject.desc);
-            $(".currentWord").html(getWordToGuess(currentObject.wordToGuess));
-            maxNumGuess = currentObject.wordToGuess.length + 5; 
-            $(".TotalGuessRemaining").text(maxNumGuess);
+            wordToGuess = currentObject.wordToGuess;
+            displayWordToGuess = printWordToGuess(displayWordToGuess, wordToGuess, '');
+            $(".currentWord").html(displayWordToGuess);
+            numGuessRem = wordToGuess.length + 5;
+            $(".TotalGuessRemaining").text(numGuessRem);
         }
-        else if(isGameStarted){
+        else if (isGameStarted) {
             //collected user input and implement the core logic of the game here!
+            if (numGuessRem > 0 && dashCount(displayWordToGuess) > 0) {
+                console.log(event.key);
+                userGuess = event.key;
+                if (wordToGuess.includes(userGuess)) {
+                    displayWordToGuess = printWordToGuess(displayWordToGuess, wordToGuess, userGuess);
+                    $(".currentWord").html(displayWordToGuess);
+                    if (dashCount(displayWordToGuess) === 0){
+                        totalWins++;
+                        $('.totalWins').text(totalWins);
+                        //play sound
+                        isGameStarted = false;
+                    }
+                } else {
+                    if (!letterAlreadyGuess.includes(userGuess)) {
+                        numGuessRem--;
+                        letterAlreadyGuess.push(userGuess);
+                        $(".TotalGuessRemaining").text(numGuessRem);
+                        $(".LetterAlGuessed").text(letterAlreadyGuess.map(function (letter) { return `${letter} ` }));
 
-            
+                    }
+                }
+            }
+
+
+
         }
-        
+
     });
 
-    
+
 
 
 });
